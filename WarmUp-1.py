672650f35130e1d80c0c7db4bc384f4b454c9cde
kart2k15 <kartikey@buffalo.edu>
2018-07-11 06:41:19 +0530

    completion of WarmUp-1

--- WarmUp-1.py
+++ WarmUp-1.py
@@ -43,3 +43,43 @@ def parrot_trouble(talking, hour):
 def makes10(a, b):
     return 10 in (a, b) or sum([a, b]) == 10
 
+#Given an int n, return True if it is within 10 of 100 or 200. Note: abs(num) computes the absolute value of a number.
+def near_hundred(n):
+  return abs(n-100)<=10 or abs(n-200)<=10
+
+#Given 2 int values, return True if one is negative and one is positive. Except if the parameter "negative" is True, then return True only if both are negative.
+def pos_neg(a, b, negative):
+    if (negative == True):
+        return (a < 0 and b < 0)
+    else:
+        return (a > 0 and b < 0) or (a < 0 and b > 0)
+
+
+#Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged.
+def not_string(str):
+  l=str.split()
+  if(l[0]=='not'):
+    return str
+  else:
+    l.insert(0,'not')
+    return " ".join(l)
+
+#Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..len(str)-1 inclusive)
+def missing_char(str, n):
+  s=str[:n]+str[n+1:]
+  return s
+
+#Given a string, return a new string where the first and last chars have been exchanged.
+def front_back(str):
+  if(len(str)<=1):
+    return str
+  mid=str[1:len(str)-1]
+  return str[len(str)-1]+mid+str[0]
+
+#Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+def front3(str):
+  if(len(str)<=3):
+    return str*3
+  else:
+    return str[0:3]*3
+
