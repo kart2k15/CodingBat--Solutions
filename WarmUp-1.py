0adb8fec9222dcaea67d846df9d7122be28602e8
kart2k15 <kartikey@buffalo.edu>
2018-07-11 06:28:23 +0530

    "monkey_trouble"

--- WarmUp-1.py
+++ WarmUp-1.py
@@ -8,3 +8,16 @@ sleep_in(False, True) → True
 '''
 def sleep_in(weekday, vacation):
   return weekday==False or vacation==True
+
+'''
+We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
+
+
+monkey_trouble(True, True) → True
+monkey_trouble(False, False) → True
+monkey_trouble(True, False) → False
+'''
+def monkey_trouble(a_smile, b_smile):
+  return (a_smile==True and b_smile==True) or (a_smile==False and b_smile==False)
+
+
